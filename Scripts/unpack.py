68ca5960ab92cd7614ebd7fcd7b5ab8fe78b2ded
Victor Azzam <victorazzam@users.noreply.github.com>
2018-08-27 12:42:46 +0800

    Add files via upload
    Copy files in a nested directory tree into a single directory.

---
+++ Scripts/unpack.py
@@ -0,0 +1,16 @@
+import os, shutil, random
+from sys import argv, exit
+
+try:
+	if len(argv) != 2:
+		exit("Usage: unpack <directory>")
+	cwd = os.getcwd()
+	save = "unpacked_" + str(random.randint(1000, 10000))
+	os.makedirs(save)
+	for i in os.walk(argv[1]):
+		files = [i[0] + "/" + x for x in i[2]]
+		for f in files:
+			shutil.copyfile(f, save + "/" + f.split("/")[-1])
+	print("Files saved to " + save)
+except KeyboardInterrupt:
+	pass
