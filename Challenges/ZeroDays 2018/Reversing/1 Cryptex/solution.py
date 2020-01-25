8bd6a34af9d43897b9b3e8155014d9df12143fee
Victor Azzam <victor123uk@gmail.com>
2020-01-25 01:10:07 +0000

    Update solution.py

--- Challenges/ZeroDays 2018/Reversing/1 Cryptex/solution.py
+++ Challenges/ZeroDays 2018/Reversing/1 Cryptex/solution.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
 # Function from challenge
-a = lambda x: (((ord(x) << 2) | (ord(x) >> 5)) ^ 123) & 255
+a = lambda x: ((x << 2 | x >> 5) ^ 123) & 255
 
 # All possible values
-b = [a(chr(g)) for g in range(255)]
+b = list(map(a, range(256)))
 
 # Values from challenge
-v = [113, 220, 168, 206, 164, 220, 180, 236, 250, 73, 196, 228, 220, 244]
+c = [113, 220, 168, 206, 164, 220, 180, 236, 250, 73, 196, 228, 220, 244]
 
-print "".join(chr(b.index(i)) for i in v)
+print("".join(map(chr, map(b.index, c))))
 
 # Bit-wise Logic
 # Flag: ZD2018{Bit-wise Logic}
